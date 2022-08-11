b707888e861dc5274f230125e3671d081e7c654d
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2022-08-11 10:01:02 -0300

    Add navegacao do login para listagem de tarefas

--- app/src/main/java/br/ufjf/dcc196/yuriperro/tasker/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/yuriperro/tasker/MainActivity.java
@@ -2,7 +2,9 @@ package br.ufjf.dcc196.yuriperro.tasker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void navigateToListTasks(View source){
+        Intent intent = new Intent(MainActivity.this, ListTasksActivity.class);
+        startActivity(intent);
     }
 }
