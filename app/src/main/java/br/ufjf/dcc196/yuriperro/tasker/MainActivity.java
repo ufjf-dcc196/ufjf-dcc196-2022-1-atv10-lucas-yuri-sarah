084cfb2eb8fcba24eb6fd8390c0662e75a8b006f
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2022-08-12 22:48:08 -0300

    Acesso do banco na tela principal

--- app/src/main/java/br/ufjf/dcc196/yuriperro/tasker/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/yuriperro/tasker/MainActivity.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = AppDatabase.getInstance(getApplicationContext());
     }
 
     public void navigateToListTasks(View source){
