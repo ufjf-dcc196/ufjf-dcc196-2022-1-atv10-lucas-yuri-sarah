3f0c3713a509e50685920a5a40ac9bb311c0af96
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2022-08-12 22:47:03 -0300

    Cria app database

---
+++ app/src/main/java/br/ufjf/dcc196/yuriperro/tasker/AppDatabase.java
@@ -0,0 +1,29 @@
+package br.ufjf.dcc196.yuriperro.tasker;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+// quando for adicionar a entidade usuário {Task.class}, {User.class}
+
+@Database(entities = {Task.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public static final String DB_NAME = "tasker-db";
+    private static AppDatabase INSTANCE;
+
+    public abstract TaskDao taskDao();
+
+    public static AppDatabase getInstance(Context ctx){
+        if(INSTANCE == null){
+            INSTANCE = Room.databaseBuilder(
+                    ctx.getApplicationContext(),
+                    AppDatabase.class,
+                    DB_NAME
+            ).allowMainThreadQueries().build();
+        }
+        return INSTANCE;
+    }
+
+}
