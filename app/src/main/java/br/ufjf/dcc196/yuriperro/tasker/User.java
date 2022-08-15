6d147313285eced70395d765ccb98644d1407b4a
Yuri Baumgartner <yuribaumgartner@ice.ufjf.br>
2022-08-14 21:16:46 -0300

    add User class

---
+++ app/src/main/java/br/ufjf/dcc196/yuriperro/tasker/User.java
@@ -0,0 +1,62 @@
+package br.ufjf.dcc196.yuriperro.tasker;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "user")
+public class User {
+    @PrimaryKey(autoGenerate = true)
+    private Long id;
+
+    @ColumnInfo(name="name")
+    private String name;
+
+    @ColumnInfo(name="email")
+    private String email;
+
+    @ColumnInfo(name="password")
+    private String password;
+
+    public User() {
+        this(null,null, null);
+    }
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
